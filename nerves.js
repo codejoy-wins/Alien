b6d349f462aa0ce7e564c358219cbd2de7c554de
Max <mjann@ucsc.edu>
2019-01-07 19:49:22 -0800

    tinkering randomization

--- nerves.js
+++ nerves.js
@@ -1,3 +1,21 @@
-function SongRandomizer(){
+var master = ["Drowning.mp3", "KILLSHOT.mp3", "nextsong.mp3"];
 
+
+function SongRandomizer(){
+    console.log("hi");
+    var song;
+    var rando = Math.random();
+    console.log(rando);
+    if(rando >.5){
+        console.log("get high");
+        song = "Drowning.mp3";
+    }else{
+        song = "KILLSHOT.mp3";
+    }
+    var target = document.getElementById("musicplayer");
+    var msg = "<embed src='./"+song+"'></embed>";
+    console.log(msg);
+    if(target){
+        target.innerHTML = msg;
+    }
 }
