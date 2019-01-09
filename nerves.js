d9c79c019195b1ebbb5f786cae3b2c5599d57b17
Max <mjann@ucsc.edu>
2019-01-08 18:47:11 -0800

    added idioteque

--- nerves.js
+++ nerves.js
@@ -1,16 +1,21 @@
-var master = ["Drowning.mp3", "KILLSHOT.mp3", "nextsong.mp3"];
-
+var master = ["Drowning.mp3", "KILLSHOT.mp3", "radiohead.mp3"];
+// for now doing without master
 
 function SongRandomizer(){
     console.log("hi");
     var song;
     var rando = Math.random();
     console.log(rando);
-    if(rando >.5){
-        console.log("get high");
+    if(rando >.33 && rando <.66){
+        console.log("stop looking at my code");
         song = "Drowning.mp3";
+
+    }else if(rando > .66){
+        console.log("hi");
+        song = "radiohead.mp3";
     }else{
         song = "KILLSHOT.mp3";
+
     }
     var target = document.getElementById("musicplayer");
     var msg = "<embed src='./"+song+"'></embed>";
